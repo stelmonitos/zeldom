c7bbb90321bc1dbfc9b6107e26a5247f0075250f
Jakub Stelmasiak <stelmon3@gmail.com>
2025-03-29 19:14:09 +0100

    add nav & splash

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', hbs.engine());
+app.set('view engine', '.hbs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+});
