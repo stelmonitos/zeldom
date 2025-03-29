59fb3dee01850fbf3b2c87ec3521bbc1a378a5e6
Jakub Stelmasiak <stelmon3@gmail.com>
2025-03-29 20:10:19 +0100

    add about fix scss

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => {
     res.render('home');
 });
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
